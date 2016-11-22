252ed989bde42f69cb4accbeb58d38c08315f62d
ta-chibana <tachibana.gmc@gmail.com>
2016-11-22 09:12:54 +0900

    Modify main.js

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 window.onload = function() {
+  var Game = function() {
+    this.turn = 1;
+  };
+
+  Game.prototype = {
+    changeTurn: function() {
+      this.turn *= -1;
+    }
+  }
+
   var Board = function() {
     this.stones = Board.create();
   };
@@ -19,6 +29,8 @@ window.onload = function() {
         td.className = 'column' + column;
 
         var cell = document.createElement('div');
+        cell.dataset.row = row;
+        cell.dataset.column = column;
         cell.className = 'cell';
         cell.id = 'cell' + row + column;
 
@@ -66,16 +78,75 @@ window.onload = function() {
         });
       });
     },
-    scanAllLine: function() {
+    scanAllLine: function(stone, callback) {
+      var directions = [
+        { vertical: -1, horizontal: 0 },
+        { vertical: -1, horizontal: 1 },
+        { vertical:  0, horizontal: 1 },
+        { vertical:  1, horizontal: 1 },
+        { vertical:  1, horizontal: 0 },
+        { vertical:  1, horizontal: -1 },
+        { vertical:  0, horizontal: -1 },
+        { vertical: -1, horizontal: -1 }
+      ];
+
+      var targetStones = [];
+      directions.forEach(function(direction) {
+        var distance = 1;
+
+        // 行毎の処理は切り出したい
+        var tmpTargetStones = [];
+
+        var relativeRow = direction.vertical;
+        var relativeColumn = direction.horizontal;
+        function relativePosition(distance) {
+          relativeRow = direction.vertical * distance;
+          relativeColumn = direction.horizontal * distance;
+          return { row: relativeRow, column: relativeColumn };
+        }
+
+        var firstTarget = this.findStoneByRelative(stone, relativePosition(distance));
+
+        // 長い
+        if ((!firstTarget || firstTarget.isNone() || !firstTarget.isDifferentColor(stone))) {
+          return;
+        }
+
+        tmpTargetStones.push(firstTarget);
+        distance++;
+
+        var targetStone;
+        while (targetStone = this.findStoneByRelative(stone, relativePosition(distance))) {
+          if (targetStone.isNone()) {
+            return;
+          } else if (targetStone.isDifferentColor(stone)) {
+            tmpTargetStones.push(targetStone);
+            distance++;
+          } else {
+            targetStones = targetStones.concat(tmpTargetStones);
+            return;
+          }
+        }
+      }, this);
+      return targetStones;
     },
     scanLine: function() {
+    },
+    findStoneByRelative: function(stone, relative) {
+      var row = stone.row + relative.row;
+      var column = stone.column + relative.column;
+      if (this.stones[row]) {
+        return this.stones[row][column];
+      } else {
+        return null;
+      }
     }
   };
 
-  var Stone = function(row, column) {
-    this.state = 0;
+  var Stone = function(row, column, state) {
     this.row = row;
     this.column = column;
+    this.state = state || 0;
   };
 
   Stone.prototype = {
@@ -88,8 +159,18 @@ window.onload = function() {
     toWhite: function() {
       this.state = -1;
     },
+    toTurnsColor: function(game) {
+      this.state = game.turn;
+      return this;
+    },
     reverse: function() {
       this.state *= -1;
+    },
+    isNone: function() {
+      return this.state == 0;
+    },
+    isDifferentColor: function(other) {
+      return this.state == other.state * -1;
     },
     display: function() {
       var states = {
@@ -101,13 +182,36 @@ window.onload = function() {
     }
   };
 
+  var game = new Game();
   var board = new Board().init();
 
   (function() {
     var cells = document.getElementsByClassName('cell');
     Array.prototype.forEach.call(cells, function(cell) {
       cell.addEventListener('click', function() {
-        console.log('success!!');
+        var row = this.dataset.row;
+        var column = this.dataset.column;
+        var stone = board.stones[row][column];
+        if (!stone.isNone()) {
+          console.log('すでに置かれているよ');
+          return;
+        }
+        console.log('λ..');
+
+        var tmpStone = new Stone(stone.row, stone.column, game.turn);
+        var targets = board.scanAllLine(tmpStone);
+        if (targets.length > 0) {
+          stone.toTurnsColor(game);
+          targets.forEach(function(s) {
+            s.reverse();
+          });
+          board.update();
+        } else {
+          console.log('no changed..')
+          return;
+        }
+
+        game.changeTurn();
       });
     });
   })();
